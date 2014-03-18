c500e39d52e5343888838b06de7ce22b8783f069
Miguel Fernandes <migpfernandes@gmail.com>
2014-03-18 01:17:58 +0000

    Adição da classe NeighbourDiscoverer responsável por no arranque de sistema descobrir os vizinhos diretos e de raio 2.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 cc_adhoc
 ========
+
+Ferramentas utilizadas
+========
+- GSN3 - emulador de rede
+- GSON - conversor de e para json de objetos
--- adhoc_app/src/Functions/NeighbourDiscoverer.java
+++ adhoc_app/src/Functions/NeighbourDiscoverer.java
@@ -6,10 +6,41 @@
 
 package Functions;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author migpfernandes
  */
 public class NeighbourDiscoverer {
+    public final int PORT = 9999;
+    public final String MCAST_ADDR = "FF02::1";
+    public final int TTL = 1;
+    
+    public NeighbourDiscoverer(){
+    }
+    
+    public void InitDiscovery() throws UnknownHostException, IOException{
+        // join a Multicast group and send the group salutations ... 
+        String msg = "Hello";
+        InetAddress group = InetAddress.getByName(MCAST_ADDR);
+        MulticastSocket s = new MulticastSocket(PORT); 
+        s.joinGroup(group);
+        s.setTimeToLive(TTL);
+        DatagramPacket hi = new DatagramPacket(msg.getBytes(), msg.length(), group, PORT);
+        s.send(hi);
+        
+        // get their responses!
+        byte[] buf = new byte[1000];
+        DatagramPacket recv = new DatagramPacket(buf, buf.length); 
+        s.receive(recv);
+        
+        // OK, I'm done talking - leave the group...
+        s.leaveGroup(group);
+    }
     
 }
